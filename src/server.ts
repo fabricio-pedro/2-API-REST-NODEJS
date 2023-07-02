8427431bd66cc2717f70064994bb704d1debf2cc
Fabricio <wolfxzero2@gmail.com>
2023-07-02 18:42:26 -0300

    alteracao no arquivo server.ts

--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,8 @@ import { knex } from './database'
 const app = fastify()
 
 app.get('/hello', async () => {
-  const test = await knex('sqlite_schema').select('*')
-  return test
+  const table = await knex('sqlite_schema').select('*')
+  return table
 })
 app
   .listen({
